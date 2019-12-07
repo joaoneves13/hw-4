08320cc1ee65b649417677b25fcf63259c5810e6
João Neves <13joaoneves@gmail.com>
2019-12-07 20:30:12 +0100

    last changes

--- exercise-2/model.js
+++ exercise-2/model.js
@@ -1,33 +1,35 @@
 const Sequelize = require("sequelize");
-const db = require('./db')
+const db = require("./db");
 
 const Movies = db.define("Movies", {
-    title: Sequelize.TEXT,
-    yearOfRelease: Sequelize.NUMBER,
-    synopsis: Sequelize.TEXT
-  });
-  
-  db
-  .sync() 
+  title: Sequelize.STRING,
+  yearOfRelease: Sequelize.INTEGER,
+  synopsis: Sequelize.STRING
+});
+
+db.sync()
   .then(() =>
     Promise.all([
       Movies.create({
         title: "Toy Story 4",
         yearOfRelease: 2019,
-        synopsis: "When a new toy called Forky joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy."
+        synopsis:
+          "When a new toy called Forky joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy."
       }),
       Movies.create({
         title: "Blow",
         yearOfRelease: 2001,
-        synopsis: "The story of how George Jung, along with the Medellín Cartel headed by Pablo Escobar, established the American cocaine market in the 1970s in the United States."
+        synopsis:
+          "The story of how George Jung, along with the Medellín Cartel headed by Pablo Escobar, established the American cocaine market in the 1970s in the United States."
       }),
       Movies.create({
         title: "Django",
         yearOfRelease: 2012,
-        synopsis: "With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner."
+        synopsis:
+          "With the help of a German bounty hunter, a freed slave sets out to rescue his wife from a brutal Mississippi plantation owner."
       })
     ])
   )
-  .catch(console.error)
+  .catch(console.error);
 
-module.exports = Movies
+module.exports = Movies;
