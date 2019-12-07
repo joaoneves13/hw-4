8472e93b8b188cbdd0022b701b1a9b5edfbea98b
João Neves <13joaoneves@gmail.com>
2019-12-07 20:25:42 +0100

    exercise line 6 and 7

--- exercise-1/messages-api.js
+++ exercise-1/messages-api.js
@@ -3,12 +3,25 @@ const bodyParser = require("body-parser");
 const port = 3000;
 const app = express();
 
+let messagesCount = 0;
+
+const countMessagesLimit = (req, res, next) => {
+  messagesCount++;
+    
+  if (messagesCount > 5) {
+    return res.status(429).send();
+  }
+  next();
+};
+
+app.use(countMessagesLimit);
 app.use(bodyParser.json());
 
 app.post("/messages", (req, res) => {
-  if (!req.body.text || req.body.text === " ") 
-  return res.status(400).send();
-  console.log(req.body);
+  if (!req.body.text || req.body.text === " ") {
+    return res.status(400).send("You should send a 'text' field");
+  }
+  console.log(req.body.text);
   res.json({
     message: "Message received loud and clear"
   });
